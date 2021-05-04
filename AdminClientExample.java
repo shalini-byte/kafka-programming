918c8a488cccab1783796e9b0f4bb53851c770ac
shalini-byte <53972000+shalini-byte@users.noreply.github.com>
2021-05-04 11:36:40 +0530

    Update AdminClientExample.java

--- AdminClientExample.java
+++ AdminClientExample.java
@@ -20,89 +20,133 @@ import org.slf4j.LoggerFactory;
 
 public class AdminClientExample {
 
-    private static Logger logger = LoggerFactory.getLogger(AdminClientExample.class);
+    private static final Logger logger = LoggerFactory.getLogger(AdminClientExample.class);
 
-    //private static Collection<String> TOPIC_LIST=Collections.singleton("sample2");
-    private static Collection<String> TOPIC_LIST= Arrays.asList("CustomerCountry", "MyCountry");
+    private static final Collection<String> TOPIC_LIST = Arrays.asList("CustomerCountry", "Products", "Sample");
+
+    private static final Collection<String> TOPIC_LIST_DELETION = Arrays.asList("Sample", "Products");
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-        AdminClient adminClient=getAdminClient();
-        AdminClientExample adminClientExample=new AdminClientExample();
-       adminClientExample.listTopics(adminClient);
-        //adminClientExample.describeTopics(adminClient,"CustomerCountry");
-       //createTopic("NewCountry",1,(short) 1);
-       // adminClientExample.deleteTopic(adminClient,"test");
+        //creating instance of AdminClient class
+        AdminClient adminClient = getAdminClient();
+
+        //Creating instance of AdminClientExample Class
+        AdminClientExample adminClientExample = new AdminClientExample();
+
+        //listing topics present in the cluster
+        adminClientExample.listTopics(adminClient);
+        //describe topic of the cluster
+        adminClientExample.describeTopics(adminClient, "CustomerCountry");
+        //create topic in the cluster
+        adminClientExample.createTopic(adminClient,"NewCountry", 1, (short) 1);
+        //delete topics present in the cluster
+        adminClientExample.deleteTopic(adminClient, "Products");
     }
 
+    /**
+     * This method creates and returns an instance of AdminClient class with necessary configurations.
+     *
+     * @return AdminClient class instance with configurations as provided in the Properties.
+     */
     private static AdminClient getAdminClient() {
 
-        Properties props = new Properties();
-        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        AdminClient adminClient = AdminClient.create(props);
+        Properties properties = new Properties();
+        properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        AdminClient adminClient = AdminClient.create(properties);
         return adminClient;
     }
 
-    public void listTopics(AdminClient adminClient) throws ExecutionException, InterruptedException {
+    /**
+     * This method lists all topics in the cluster and print topic names by iterating over the list.
+     *
+     * @param adminClient AdminClient class instance
+     */
+    public void listTopics(final AdminClient adminClient) throws ExecutionException, InterruptedException {
 
         ListTopicsResult topics = adminClient.listTopics();
         topics.names().get().forEach(System.out::println);
+        adminClient.close(Duration.ofSeconds(30));
     }
 
-    public void describeTopics(AdminClient adminClient, final String topicName) throws ExecutionException, InterruptedException {
+    /**
+     * This method describes and validates the configuration of the topic if it exists otherwise creates new topic by
+     * handling the exception.
+     *
+     * @param adminClient AdminClient class instance
+     * @param topicName   Topic name to be described
+     */
+    public void describeTopics(AdminClient adminClient, final String topicName)
+            throws ExecutionException, InterruptedException {
 
-        DescribeTopicsResult demoTopic=adminClient.describeTopics(TOPIC_LIST);
+        final DescribeTopicsResult demoTopic = adminClient.describeTopics(TOPIC_LIST);
         try {
             final TopicDescription topicDescription = demoTopic.values().get(topicName).get();
             System.out.println("Description of demo topic:" + topicDescription);
 
-          /*  if (topicDescription.partitions().size() != 2) {
+            if (topicDescription.partitions().size() != 1) {
                 System.out.println("Topic has wrong number of partitions. Exiting.");
                 System.exit(-1);
-            }*/
-        }
-        catch (ExecutionException e){
-            if (! (e.getCause() instanceof UnknownTopicOrPartitionException)) {
-                e.printStackTrace();
-                throw e;
             }
-            System.out.println("Topic " + topicName + " does not exist. Going to create it now");
-            createTopic(topicName,1, (short) 1);
+        } catch (ExecutionException e) {
+            if (!(e.getCause() instanceof UnknownTopicOrPartitionException)) {
+                System.out.println("Topic " + topicName + " does not exist. It will be created on spot");
+                createTopic(adminClient, topicName, 1, (short) 1);
+            }
         }
-
-
+        adminClient.close(Duration.ofSeconds(30));
     }
 
-    public static void createTopic(final String topicName, final int partitions, final short replicationFactor){
-        try(final AdminClient adminClient = getAdminClient()){
+    /**
+     * This method creates new topic with the necessary configuration as provided by the parameters and throws
+     * exception if the topic name provided already exists.
+     *
+     * @param adminClient       AdminClient class instance
+     * @param topicName         name of the topic to be created
+     * @param partitions        number of partitions in the new topic
+     * @param replicationFactor replication factor for the new topic
+     */
+    public void createTopic(final AdminClient adminClient, final String topicName, final int partitions, final short replicationFactor)
+            throws ExecutionException, InterruptedException {
+
+        try {
+            logger.info("The number of partitions and replicas are optional. If there are not specified, "
+                    + "the defaults configured on the Kafka brokers will be used");
             final NewTopic newTopic = new NewTopic(topicName, partitions, replicationFactor);
             final CreateTopicsResult createTopicsResult = adminClient.createTopics(Collections.singleton(newTopic));
-            createTopicsResult.values().get(topicName).get();
-            adminClient.close(Duration.ofSeconds(30));
-        }
+       } 
         catch (InterruptedException | ExecutionException e) {
             if (!(e.getCause() instanceof TopicExistsException)) {
-                throw new RuntimeException(e.getMessage(), e);
+                DescribeTopicsResult describeTopics = adminClient.describeTopics(Collections.singleton(topicName));
+                TopicDescription topicDescription = describeTopics.values().get(topicName).get();
+                System.out.println("Description of topic: " + topicName + " " + topicDescription);
             }
-            e.printStackTrace();
         }
-
+        adminClient.close(Duration.ofSeconds(30));
     }
 
-    public  void deleteTopic(AdminClient adminClient,final String topicName) throws ExecutionException, InterruptedException {
-        adminClient.deleteTopics(TOPIC_LIST).all().get();
-        DeleteTopicsResult d=adminClient.deleteTopics(TOPIC_LIST);
-       /* try {
-            DescribeTopicsResult demoTopic=adminClient.describeTopics(TOPIC_LIST);
-            final TopicDescription topicDescription = demoTopic.values().get(topicName).get();
-            System.out.println("Topic " + topicName + " is still around");
+    /**
+     * This method deletes the topics provided in the topic list and checks if the topic has been deleted.
+     *
+     * @param adminClient AdminClient class instance
+     * @param topicName   Topic name to be checked for deletion
+     */
+    public void deleteTopic(final AdminClient adminClient, final String topicName) throws ExecutionException,
+            InterruptedException {
+
+        DeleteTopicsResult deleteTopicsResult = adminClient.deleteTopics(TOPIC_LIST_DELETION);
+        deleteTopicsResult.all().get();
+
+        logger.info("Check that it is deleted. Note that due to the async nature of deletes, it is possible that at "
+                + "this point the topic still exists");
+        try {
+            DescribeTopicsResult demoTopic = adminClient.describeTopics(TOPIC_LIST);
+            TopicDescription topicDescription = demoTopic.values().get(topicName).get();
+            System.out.println("Topic " + topicName + " is still present");
         } catch (ExecutionException e) {
-            System.out.println("Topic " + topicName + " is gone");
-        }*/
-
+            System.out.println("Topic " + topicName + " is deleted");
+        }
+        adminClient.close(Duration.ofSeconds(30));
     }
-
-
 
 }
-
